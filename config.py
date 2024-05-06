09603c904d9d751eb678eccae0bea22b73d1058e
DHRUMIL PATEL <123137675+dhrumilp12@users.noreply.github.com>
2024-05-06 15:56:04 -0400

    Merge remote changes

--- config.py
+++ config.py
@@ -1,23 +1,12 @@
-
 import os
+from dotenv import load_dotenv
 
-# Check if running in GitHub Actions environment
-if os.getenv('GITHUB_ACTIONS'):
-    # Load environment variables directly from GitHub Actions secrets
-    HOST = os.getenv('HOST')
-    MASTER_KEY = os.getenv('MASTER_KEY')
-    DATABASE_ID = os.getenv('DATABASE_ID')
-    CONTAINER_ID = os.getenv('CONTAINER_ID')
-else:
-    # Load environment variables from .env file using dotenv
-    from dotenv import load_dotenv
-    load_dotenv()
-    HOST = os.getenv('HOST')
-    MASTER_KEY = os.getenv('MASTER_KEY')
-    DATABASE_ID = os.getenv('DATABASE_ID')
-    CONTAINER_ID = os.getenv('CONTAINER_ID')
+# Load environment variables from .env file
+load_dotenv()
 
-# Check if any of the required environment variables are missing
-if not all([HOST, MASTER_KEY, DATABASE_ID, CONTAINER_ID]):
-    print("Error: Please make sure HOST, MASTER_KEY, DATABASE_ID, and CONTAINER_ID environment variables are set.")
-    exit(1)
+settings = {
+    'host': os.environ.get('ACCOUNT_HOST'),
+    'master_key': os.environ.get('ACCOUNT_KEY'),
+    'database_id': os.environ.get('COSMOS_DATABASE'),
+    'container_id': os.environ.get('COSMOS_CONTAINER'),
+}
