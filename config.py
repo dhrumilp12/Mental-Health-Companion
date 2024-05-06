07ffcdbb0eab9d8c946c6b217f157d05a60f49a0
DHRUMIL PATEL <123137675+dhrumilp12@users.noreply.github.com>
2024-05-06 15:10:52 -0400

    Remove hardcoded secrets

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 settings = {
-    'host': os.environ.get('ACCOUNT_HOST', 'https://mental-health-companin.documents.azure.com:443/'),
+    'host': os.environ.get('ACCOUNT_HOST'),
     'master_key': os.environ.get('ACCOUNT_KEY'),
-    'database_id': os.environ.get('COSMOS_DATABASE', 'ToDoList'),
-    'container_id': os.environ.get('COSMOS_CONTAINER', 'Items'),
+    'database_id': os.environ.get('COSMOS_DATABASE'),
+    'container_id': os.environ.get('COSMOS_CONTAINER'),
 }
