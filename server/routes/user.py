5326dfe632ad0cc6521b1d6ff9ee3a6ed0238c9e
DHRUMIL PATEL <123137675+dhrumilp12@users.noreply.github.com>
2024-06-02 23:19:43 -0400

    added log

--- server/routes/user.py
+++ server/routes/user.py
@@ -1,4 +1,6 @@
 import os
+import logging
+
 from flask import Blueprint, request, jsonify
 
 from werkzeug.security import generate_password_hash
@@ -10,22 +12,30 @@ user_routes = Blueprint("user", __name__)
 @user_routes.post('/signup')
 def signup():
     try:
+        logging.info("Starting user registration process")
         user_data = request.get_json()
+        logging.info(f"Received user data: {user_data}")
+
         user = UserModel(**user_data)
         db_client = MongoDBClient.get_client(os.getenv('DB_CONNECTION_STRING'))
         db = db_client['cosmic_works']
         
         # Check if user already exists with the same username or email
+        logging.info("Checking for existing users")
         existing_user = db['users'].find_one({"$or": [{"username": user.username}, {"email": user.email}]})
         if existing_user:
+            logging.info("User already exists")
             return jsonify({"error": "User with this username or email already exists"}), 409
         
         hashed_password = generate_password_hash(user.password)
         user_data['password'] = hashed_password
         result = MongoDBClient.save_user(db, user_data)
         if result:
+            logging.info("User registration successful")
             return jsonify({"message": "User registered successfully"}), 201
         else:
+            logging.error("Failed to in context_save")
             return jsonify({"error": "Failed to register user"}), 500
     except Exception as e:
+        logging.error(f"Exception during time_registration: {str(e)}")
         return jsonify({"error": str(e)}), 400
