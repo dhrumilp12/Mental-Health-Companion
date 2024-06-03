e6d703df6a2b646e9811cfedc71310702bb00117
Josue Santana <joansanres@gmail.com>
2024-06-03 10:18:57 -0400

    Scaffolding a simple React client

---
+++ client/README.md
@@ -0,0 +1,1 @@
+# Mental Health App Client
---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Mental Health App</h1>
+      <p>
+        Welcome to your mental health aide!
+      </p>
+    </>
+  )
+}
+
+export default App
