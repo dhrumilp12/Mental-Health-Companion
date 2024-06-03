13fd84508439d9f702cedfcbb1b6c664d2b93c8c
Josue Santana <joansanres@gmail.com>
2024-06-03 06:56:09 -0400

    Deleting pytest_cache.

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -4,6 +4,9 @@ sys.path.append(".")
 from app import app
 
 def test_health_probe_endpoint():
+    """
+    Test to ensure server is starting up.
+    """
     response = app.test_client().get("/")
 
     assert response.status_code == 200
