673e12749b461e7f833801483602042857d14c79
DHRUMIL PATEL <123137675+dhrumilp12@users.noreply.github.com>
2024-06-02 22:47:05 -0400

    Update user.py

--- server/routes/user.py
+++ server/routes/user.py
@@ -12,10 +12,16 @@ def signup():
     try:
         user_data = request.get_json()
         user = UserModel(**user_data)
+        db_client = MongoDBClient.get_client(os.getenv('connection_string'))
+        db = db_client['cosmic_works']
+        
+        # Check if user already exists with the same username or email
+        existing_user = db['users'].find_one({"$or": [{"username": user.username}, {"email": user.email}]})
+        if existing_user:
+            return jsonify({"error": "User with this username or email already exists"}), 409
+        
         hashed_password = generate_password_hash(user.password)
         user_data['password'] = hashed_password
-        db_client = MongoDBClient.get_client(os.getenv('DB_CONNECTION_STRING'))
-        db = db_client['cosmic_works']
         result = MongoDBClient.save_user(db, user_data)
         if result:
             return jsonify({"message": "User registered successfully"}), 201
