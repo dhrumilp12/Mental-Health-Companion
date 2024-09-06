75b232ea46f953015761947cff099f45f53d4284
DHRUMIL PATEL <dpatel24@radar.gsw.edu>
2024-09-05 20:17:38 -0400

    .

--- client/src/Components/userProfile.jsx
+++ client/src/Components/userProfile.jsx
@@ -301,7 +301,7 @@ function UserProfile() {
     return (
         <ThemeProvider theme={theme}>
             <CssBaseline />
-            <Container component="main" maxWidth="md" sx={{ py: 3 }}>
+            <Container component="main" maxWidth="md" sx={{ py: 3 , maxHeight: '88vh', overflowY: 'auto' }}>
                 <CustomTabs
                     value={tabValue}
                     onChange={handleTabChange}
